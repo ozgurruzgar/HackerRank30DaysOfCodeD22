af5a0375ac16cd3add2456527ae56c6ea67eafb9
Özgür <zgr@DESKTOP-1NLBACI>
2022-12-18 01:28:37 +0300

    Add project files.

---
+++ HackerRank30DaysOfCodeD22/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace HackerRank30DaysOfCodeD22
+{
+    class Node
+    {
+        public Node left, right;
+        public int data;
+        public Node(int data)
+        {
+            this.data = data;
+            left = right = null;
+        }
+    }
+    class Program
+    {
+        static int getHeight(Node root)
+        {
+            if (root == null)
+            {
+                return -1;
+            }
+
+            int leftHeight = getHeight(root.left);
+            int rightHeight = getHeight(root.right);
+
+            if (leftHeight > rightHeight)
+            {
+                return leftHeight + 1;
+            }
+            else
+            {
+                return rightHeight + 1;
+            }
+        }
+
+        static Node insert(Node root, int data)
+        {
+            if (root == null)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node cur;
+                if (data <= root.data)
+                {
+                    cur = insert(root.left, data);
+                    root.left = cur;
+                }
+                else
+                {
+                    cur = insert(root.right, data);
+                    root.right = cur;
+                }
+                return root;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Node root = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                root = insert(root, data);
+            }
+            int height = getHeight(root);
+            Console.WriteLine(height);
+        }
+    }
+}
